aaf43099d4ea4441dc1e90da9b1a71e534c88486
contraintuitiv <mail@contraintuitiv.de>
2022-03-06 22:30:50 +0100

    first part phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,75 @@
 import { useState } from 'react'
 
+const initialpersons = [
+  { name: 'Arto Hellas' }
+]
+
+const Filter = ({handleChange}) => <div>filter by: <input onChange={handleChange} /></div>
+const Person = ({person}) => <li>{person.name}<br /><i>{person.number}</i></li>
+
+const PersonForm = (props) => {
+  return(<form>
+    <div>
+      name: <input value={props.newName} onChange={props.handleNameChange} /><br />
+      number: <input value={props.newNumber} onChange={props.handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit" onClick={props.addPerson}>add</button>
+    </div>
+  </form>)
+}
+
+const Persons = ({persons}) => {
+  return(
+  <ul>
+    {persons.map((person) => <Person key={person.name} person={person} />)}
+  </ul>)
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState(initialpersons) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if(persons.find((person) => person.name==newName)){
+      alert (`${newName} is already in the list`)
+    }else{
+      setPersons(persons.concat({name: newName, number: newNumber}))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+  const handleFilterChange = (event) => {    
+    setFilter(event.target.value)
+  }
+
+  const shownPersons = persons.filter(
+      (person) => person.name.toLowerCase().includes(filter.toLocaleLowerCase())
+    )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter handleChange={handleFilterChange} />
+      
+      <h3>add new person</h3>
+      <PersonForm 
+        newName={newName} 
+        newNumber={newNumber} 
+        handleNameChange={handleNameChange} 
+        handleNumberChange={handleNumberChange} 
+        addPerson={addPerson}
+      />
+
       <h2>Numbers</h2>
-      ...
+      <Persons persons={shownPersons} />
     </div>
   )
 }
